ab2723c7569ad8bca95d706063f7d6389dac0849
Dim369322 <garnazhenkobb@gmail.com>
2022-07-05 13:48:00 +0200

    upd

---
+++ src/components/App.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Phonebook } from './app.styled';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onFormSubmit = evt => {
+    evt.preventDefault();
+
+    const { name, number } = evt.target.elements;
+
+    this.setState(prevState => {
+      const contact = {
+        id: nanoid(),
+        name: name.value,
+        number: number.value,
+      };
+
+      const filteredContact = prevState.contacts.find(
+        ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+      );
+
+      return filteredContact
+        ? alert(`${contact.name} is already in contacts`)
+        : {
+            name: name.value.toLowerCase(),
+            contacts: [contact, ...prevState.contacts],
+          };
+    });
+  };
+
+  changeFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value.trim() });
+  };
+
+  removeContact = id => {
+    const filtered = this.state.contacts.filter(contact => contact.id !== id);
+
+    this.setState({
+      name: '',
+      contacts: filtered,
+    });
+  };
+
+  render() {
+    const normalizeText = this.state.filter.toLowerCase();
+
+    const filteredContact = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeText)
+    );
+
+    return (
+      <Phonebook>
+        <h1>Phonebook</h1>
+        <ContactForm onFormSubmit={this.onFormSubmit} />
+
+        <h2>Contacts</h2>
+        <Filter onChangeFilter={this.changeFilter} />
+        <ContactList
+          contacts={filteredContact}
+          onRemoveContact={this.removeContact}
+        />
+      </Phonebook>
+    );
+  }
+}
+
+export default App;
